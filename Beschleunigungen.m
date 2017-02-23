3e68301d405d34a5fbac2ce5e96f80d913ce3c74
moritzkoerber <moritz.koerber@tum.de>
2017-02-23 12:22:03 +0100

    changed stepsize to 0.25

--- Beschleunigungen.m
+++ Beschleunigungen.m
@@ -21,7 +21,7 @@ for i = [2 4 6 7 12 15 16 17 18 19 23 24 25 27 28 32 34 37 39 40]
     end
     
     
-    Ende = find(abs(Streckenmeter-8333) < 0.12 );
+    Ende = find(abs(Streckenmeter-8333) < 0.25 );
 
     if size(Ende,1) > 1
         Ende_row = Ende(size(Ende,1)-1,1);
